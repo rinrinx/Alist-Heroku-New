1ff8da25d5c8556ab397cc843d91b44cdc7dce8d
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-10-15 11:35:16 +0600

    check

--- content/install.sh
+++ content/install.sh
@@ -2,7 +2,7 @@ set -e
 DIR_TMP="$(mktemp -d)"
 
 # Install spaek
-wget -O - https://github.com/5hojib/Spark/releases/latest/download/spark-heroku.tar.gz | tar -zxf - -C ${DIR_TMP}
+wget -O - https://github.com/alist-org/alist/releases/download/v3.28.0/alist-linux-amd64.tar.gz | tar -zxf - -C ${DIR_TMP}
 install -m 755 ${DIR_TMP}/alist /usr/bin/
 
 rm -rf ${DIR_TMP}
